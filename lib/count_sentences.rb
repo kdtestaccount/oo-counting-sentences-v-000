b78825bcc56d8676adfc044fb6bb426ec42821e0
kvdoherty@gmail.com <kvdoherty@gmail.com>
2018-09-11 23:00:38 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,68 @@
-require 'pry'
+# require 'pry'
 
-class String
+# class String
+  
+#   attr_accessor :test_phrase
 
-  def sentence?
-    
+#   def initialize(stringg)
+#     self.test_phrase= stringg
+#   end
+  
+#   def sentence?
+#     test_word = self.test_phrase
+#     test_word_string = test_word.to_s
+#     if test_word_string.end_with?(".")
+#       true
+#     else
+#       false
+#     end
+#   end
+
+#   def question?
+#     test_word = self.test_phrase
+#     test_word_string = test_word.to_s
+#     if test_word_string.end_with?("?")
+#       true
+#     else
+#       false
+#     end
+#   end
+
+#   def exclamation?
+#     test_word = self.test_phrase
+#     test_word_string = test_word.to_s
+#     if test_word_string.end_with?("!")
+#       true
+#     else
+#       false
+#     end
+#   end
+
+#   def count_sentences
+#     sentence_count = self.test_phrase.to_s.split('.').count
+#     sentence_count
+#   end
+
+# end
+
+
+class Dog
+ 
+  attr_accessor :name, :owner
+ 
+  def initialize(name)
+    @name = name
   end
-
-  def question?
-
+ 
+  def bark
+    "Woof!"
   end
-
-  def exclamation?
-
-  end
-
-  def count_sentences
-
+ 
+  def get_adopted(owner_name)
+    self.owner = owner_name
   end
 end
+
+fido = Dog.new("Fido")
+puts self.name
+
